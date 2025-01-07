2c09bdd8cd7e0730e20478a2b4d17f29c5adc157
moloud <moloud.ayat@gmail.com>
2025-01-07 18:43:34 +0330

    style: filter

---
+++ src/components/filter-button/filter-button.component.tsx
@@ -0,0 +1,29 @@
+import { ReactElement, ComponentProps } from "react";
+
+import clsx from "clsx";
+
+import styles from "./filter-button.module.css";
+
+type Props = ComponentProps<"button"> & {
+  isActive?: boolean;
+};
+
+export default function FilterButtonComponent({
+  className,
+  children,
+  isActive = false,
+  ...otherProps
+}: Props): ReactElement {
+  return (
+    <button
+      className={clsx(
+        styles["filter-button"],
+        isActive && styles.active,
+        className,
+      )}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+}
