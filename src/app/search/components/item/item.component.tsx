15d33f2ba5fca0ab58325317e45aebd232b61f16
moloud <moloud.ayat@gmail.com>
2025-01-12 21:45:18 +0330

    feat(filter): active item functionality

--- src/app/search/components/item/item.component.tsx
+++ src/app/search/components/item/item.component.tsx
@@ -1,5 +1,7 @@
 "use client";
-import { ReactElement, useContext } from "react";
+import { ReactElement, useContext, useMemo } from "react";
+
+import clsx from "clsx";
 
 import { FiltersContext } from "@/app/search/providers/filters/filters.provider";
 
@@ -8,10 +10,20 @@ import styles from "./item.module.css";
 type Props = {
   item: number;
 };
+
 export default function ItemComponent({ item }: Props): ReactElement {
   const { filters } = useContext(FiltersContext);
+
+  const isActive = useMemo(() => {
+    if (filters.even && item % 2 === 0) return true;
+    if (filters.odd && item % 2 === 1) return true;
+    if (filters.three && item % 3 === 0) return true;
+    if (filters.five && item % 5 === 0) return true;
+    return !!(filters.seven && item % 7 === 0);
+  }, [filters, item]);
+
   return (
-    <li key={item} className={styles.item}>
+    <li key={item} className={clsx(styles.item, isActive && styles.active)}>
       {item}
     </li>
   );
